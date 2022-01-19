61921844886f7a71bea042d9e398d3106d95b92e
Tim Staudenmaier <timstaudenmaier07@gmail.com>
2022-01-19 15:42:23 +0100

    Fix PauseMenu always open

--- Assets/UI/IngameOverlay/PauseMenu.cs
+++ Assets/UI/IngameOverlay/PauseMenu.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour {
+    private GameObject Container;
+
+    void Start() {
+        Container = transform.GetChild(0).gameObject;
+    }
+
     void Update() {
         if (Input.GetKeyDown(KeyCode.Escape))
-            PauseGame();
+            if (Container.activeSelf)
+                ResumeGame();
+            else
+                PauseGame();
     }
 
     public void PauseGame() {
         Time.timeScale = 0;
+        Container.SetActive(true);
     }
 
     public void ResumeGame() {
         Time.timeScale = 1;
+        Container.SetActive(false);
     }
 
     public void ExitGame() {
